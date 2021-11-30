6a1d4d487fdaf7b256ebbcfd592a7d52e6b0331f
s-nes347 <kokuanes@yahoo.co.jp>
2021-11-30 13:15:17 +0900

    second

--- main.py
+++ main.py
@@ -57,4 +57,4 @@ def message_text(event):
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=True)
